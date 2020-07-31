baa5d48cbd61a66c9b996d0edf0e968ef983c414
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-07-31 15:33:38 +0500

    меняем шапку при скролле

--- src/js/common.js
+++ src/js/common.js
@@ -5,4 +5,20 @@ jQuery(function($) {
 		$('.header__menu').toggleClass('open');
 	});
 
+	$(window).scroll(function(){
+		elem = $('.header');
+
+		if ($(this).scrollTop() != 0) {
+			$('.header__phone, .header__phone a').css('color', '#000000');
+			elem.css({
+				'background-color': '#ffffff',
+				'box-shadow': '0 10px 50px rgba(0,0,0,.5)',
+				'padding': '20px 0'
+			});
+		} else {
+			$('.header__phone, .header__phone a').removeAttr('style');
+			elem.removeAttr('style');
+		}
+	});
+
 });
