ec72c5087229237ab5c90e2e1ad293abb147febe
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-07-31 15:38:22 +0500

    edit

--- src/js/common.js
+++ src/js/common.js
@@ -5,8 +5,9 @@ jQuery(function($) {
 		$('.header__menu').toggleClass('open');
 	});
 
+	elem = $('.header');
+
 	$(window).scroll(function(){
-		elem = $('.header');
 
 		if ($(this).scrollTop() != 0) {
 			$('.header__phone, .header__phone a').css('color', '#000000');
@@ -20,5 +21,14 @@ jQuery(function($) {
 			elem.removeAttr('style');
 		}
 	});
+
+	if ($(window).scrollTop() != 0) {
+		$('.header__phone, .header__phone a').css('color', '#000000');
+		elem.css({
+			'background-color': '#ffffff',
+			'box-shadow': '0 10px 50px rgba(0,0,0,.5)',
+			'padding': '20px 0'
+		});
+	}
 
 });
