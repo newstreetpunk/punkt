0c004b09d6d62018048ebe08d0ca05e442a541ec
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-08-11 10:29:47 +0500

    slider

--- src/js/common.js
+++ src/js/common.js
@@ -196,6 +196,16 @@ jQuery(function($) {
 			$('html').removeAttr('style');
 		}
 		
-	});		
+	});
+
+	$('.cases__preview').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: true,
+		vertical: true,
+		verticalSwiping: true
+	});
 
 });
